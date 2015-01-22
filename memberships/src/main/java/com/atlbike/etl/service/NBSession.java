b7d43452c75fb70bc8fca398c35a43ff0b8f1610
jett <jett@atlantabike.org>
2015-01-22 08:11:12 -0500

    Session code for home without proxy

--- memberships/src/main/java/com/atlbike/etl/service/NBSession.java
+++ memberships/src/main/java/com/atlbike/etl/service/NBSession.java
@@ -20,44 +20,46 @@ import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 public class NBSession {
-    
-    private CloseableHttpClient httpClient = HttpClients.createDefault();
-    private String loginURL = "https://atlbike.nationbuilder.com/forms/user_sessions";
-    private Closeable loginResponse;
-    
-    public void login() {
-	HttpHost proxy = new HttpHost("localhost", 3128);
-	RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
-	
-	HttpPost httpPost = new HttpPost(loginURL);
-	httpPost.setConfig(config);
-	List <NameValuePair> nvps = new ArrayList <NameValuePair>();
-	nvps.add(new BasicNameValuePair("user_session_email", "jett@atlantabike.org"));
-	nvps.add(new BasicNameValuePair("user_session_password", "bikeAngel"));
-	try {
-	    httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-	    CloseableHttpResponse loginResponse = httpClient.execute(httpPost);
-	    System.out.println(loginResponse.getStatusLine());
-	    HttpEntity loginEntity = loginResponse.getEntity();
-	    EntityUtils.consume(loginEntity);
-	} catch (UnsupportedEncodingException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (ClientProtocolException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	} finally {
-	    try {
-		if (loginResponse != null) {
-		    loginResponse.close();
+
+	private CloseableHttpClient httpClient = HttpClients.createDefault();
+	private String loginURL = "https://atlbike.nationbuilder.com/forms/user_sessions";
+	private Closeable loginResponse;
+
+	public void login() {
+		HttpHost proxy = new HttpHost("localhost", 3128);
+		RequestConfig config = RequestConfig.custom().setProxy(proxy).build();
+
+		HttpPost httpPost = new HttpPost(loginURL);
+		// httpPost.setConfig(config);
+		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+		nvps.add(new BasicNameValuePair("user_session_email",
+				"jett@atlantabike.org"));
+		nvps.add(new BasicNameValuePair("user_session_password", "bikeAngel"));
+		try {
+			httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+			CloseableHttpResponse loginResponse = httpClient.execute(httpPost);
+			System.out.println(loginResponse.getStatusLine());
+			HttpEntity loginEntity = loginResponse.getEntity();
+			EntityUtils.consume(loginEntity);
+
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				if (loginResponse != null) {
+					loginResponse.close();
+				}
+			} catch (IOException e) {
+				// ignore
+			}
 		}
-	    } catch (IOException e) {
-		// ignore
-	    }
 	}
-    }
 
 }
